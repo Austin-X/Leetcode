373c7276236716c9937e19d0ee1e7cba48732989
FLAME <xaustin03@gmail.com>
2022-10-12 11:42:32 -0400

    Update 5. Longest Palindromic Substring.cpp

--- Medium/5. Longest Palindromic Substring.cpp
+++ Medium/5. Longest Palindromic Substring.cpp
@@ -1,36 +1,37 @@
 class Solution {
 public:
+    // Dynamic programming solution
+    // Time complexity: O(N^2)
     string longestPalindrome(string s) {
-        string ans = "";
+        int n = s.length(), maxLength = 1, startingIndexOfAns = 0;
+        bool dp[n][n];
+        memset(dp, false, sizeof(dp[0][0]) * n * n);
+        
+        for (int i = 0; i < n; i ++) dp[i][i] = true;
+        for (int i = 2; i <= n; i ++) {
+            for (int j = 0; j < n; j ++) {
+                if (j + i - 1 < n) {
+                    if (i % 2 == 0) {
+                        if (i == 2) {
+                            if (s[j] == s[j + 1]) dp[j][j + 1] = true;
+                            else dp[j][j + 1] = false;
+                        } else {
+                            if (s[j] == s[j + i - 1] && dp[j + 1][j + i - 2]) dp[j][j + i - 1] = true;
+                            else dp[j][j + i - 1] = false;
+                        }
+                    } else {
+                        if (s[j] == s[j + i - 1] && dp[j + 1][j + i - 2]) dp[j][j + i - 1] = true;
+                        else dp[j][j + i - 1] = false;
+                    }
 
-        for (int i = 0; i < s.length(); i ++) {
-            // Odd length palindromes
-            string cur = s.substr(i, 1);
-            int j = i - 1, k = i + 1;
-            while (j >= 0 && k < s.length()) {
-                if (s[j] == s[k]) {
-                    cur = s.substr(j, k - j + 1);
-                    j --;
-                    k ++;
-                } else break;
-            }
-            if (cur.length() > ans.length()) ans = cur;
-
-            // Even length palindromes
-            if (i + 1 < s.length() && s[i] == s[i + 1]) {
-                string cur =  s.substr(i, 2);
-                j = i - 1; k = i + 2;
-                while (j >= 0 && k < s.length()) {
-                    if (s[j] == s[k]) {
-                        cur = s.substr(j, k - j + 1);
-                        j --;
-                        k ++;
-                    } else break;
+                    if (dp[j][j + i - 1]) {
+                        maxLength = i;
+                        startingIndexOfAns = j;
+                    }
                 }
-                if (cur.length() > ans.length()) ans = cur;
             }
         }
-
-        return ans;
+        
+        return s.substr(startingIndexOfAns, maxLength);
     }
 };
