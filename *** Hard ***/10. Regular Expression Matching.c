429ad438b23b38da82c9a3c157b7f44fe6a9ebdb
FLAME <xaustin03@gmail.com>
2022-12-05 13:08:10 -0500

    Create 10. Regular Expression Matching.c

---
+++ *** Hard ***/10. Regular Expression Matching.c
@@ -0,0 +1,24 @@
+bool isMatch(char *s, char *p) {
+    if (p[0] == '\0') return s[0] == '\0';
+    else if (p[0] != '.') {
+        if (s[0] != p[0]) return p[1] == '*' ? isMatch(s, p + 2) : false;
+        else if (p[1] == '*') {
+            int idx = 0;
+            while (s[idx] == p[0]) {
+                if (isMatch(s + idx + 1, p + 2)) return true;
+                idx ++;
+            }
+            return isMatch(s, p + 2);
+        } else return isMatch(s + 1, p + 1);
+    } else {
+        if (s[0] == '\0') return p[1] == '*' ? isMatch(s, p + 2) : false;
+        if (p[1] == '*') {
+            int idx = 0;
+            while (s[idx] != '\0') {
+                if (isMatch(s + idx + 1, p + 2)) return true;
+                idx ++;
+            }
+            return isMatch(s, p + 2);
+        } else return isMatch(s + 1, p + 1);
+    }
+}
