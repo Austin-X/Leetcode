834c71c8e4e34d8548b027d096395288c465e57f
FLAME <xaustin03@gmail.com>
2022-12-06 00:08:05 -0500

    Update 10. Regular Expression Matching.c

--- *** Hard ***/10. Regular Expression Matching.c
+++ *** Hard ***/10. Regular Expression Matching.c
@@ -1,19 +1,30 @@
 bool isMatch(char *s, char *p) {
-    if (p[0] == '\0') return s[0] == '\0';
-    else if (p[0] != '.') {
-        if (s[0] != p[0]) return p[1] == '*' ? isMatch(s, p + 2) : false;
-        else if (p[1] == '*') {
-            int idx = 0;
-            while (s[idx++] == p[0]) if (isMatch(s + idx, p + 2)) return true;
-            return isMatch(s, p + 2);
-        } else return isMatch(s + 1, p + 1);
-    } else {
-        if (s[0] == '\0') return p[1] == '*' ? isMatch(s, p + 2) : false;
-        if (p[1] == '*') {
-            if (p[2] == '.' && p[3] == '*') return isMatch(s, p + 2); 
-            int idx = 0;
-            while (s[idx++] != '\0') if (isMatch(s + idx, p + 2)) return true;
-            return isMatch(s, p + 2);
-        } else return isMatch(s + 1, p + 1);
+    int n = strlen(s), m = strlen(p), k;
+    bool dp[n + 1][m + 1];
+    for (int i = 0; i <= n; i ++) for (int j = 0; j <= m; j ++) dp[i][j] = false;
+    dp[n][m] = true;
+    for (int i = m - 1; i >= 0 && p[i] == '*'; i -= 2) dp[n][i - 1] = true;
+
+    for (int i = n - 1; i >= 0; i --) {
+        for (int j = m - 1; j >= 0; j --) {
+            if (p[j] == '*') dp[i][j] = false;
+            else if (p[j] == '.') {
+                if (j < m - 1 && p[j + 1] == '*') {
+                    for (k = i; k < n; k ++) dp[i][j] = fmax(dp[i][j], dp[k][j + 2]);
+                    dp[i][j] = fmax(dp[i][j], dp[k][j + 2]);
+                }
+                else dp[i][j] = dp[i + 1][j + 1];
+            } else {
+                if (j < m - 1 && p[j + 1] == '*') {
+                    if (p[j] == s[i]) {
+                        for (k = i; s[k] == p[j]; k ++) dp[i][j] = fmax(dp[i][j], dp[k][j + 2]);
+                        dp[i][j] = fmax(dp[i][j], dp[k][j + 2]);
+                    }
+                    else dp[i][j] = dp[i][j + 2];
+                } else if (s[i] == p[j]) dp[i][j] = dp[i + 1][j + 1];
+            }
+        }
     }
+
+    return dp[0][0];
 }
