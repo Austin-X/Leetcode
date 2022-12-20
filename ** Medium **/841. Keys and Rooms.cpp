de90e2037e7c63916569dcd809ec5b14f7529e21
FLAME <xaustin03@gmail.com>
2022-12-20 16:20:40 -0500

    Create 841. Keys and Rooms.cpp

---
+++ ** Medium **/841. Keys and Rooms.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        int n = rooms.size();
+        queue<int> q; bool vis[n]; memset(vis, false, sizeof(vis));
+        q.push(0); vis[0] = true;
+
+        while (!q.empty()) {
+            int val = q.front(); q.pop();
+            for (int i = 0; i < rooms[val].size(); i ++) if (!vis[rooms[val][i]]) { vis[rooms[val][i]] = true; q.push(rooms[val][i]); }
+        }
+        for (int i = 0; i < n; i ++) if (!vis[i]) return false;
+        return true;
+    }
+};
