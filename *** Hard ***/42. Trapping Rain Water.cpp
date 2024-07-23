1887f48bc7a5ec8ab3d50bffa2040f72bf6da0ac
Austin-X <xaustin03@gmail.com>
2024-07-22 23:12:31 -0400

    Create 42. Trapping Rain Water.cpp

---
+++ *** Hard ***/42. Trapping Rain Water.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int ans = 0;
+        stack<pair<int, int>> st;
+        for (int i = 0; i < height.size(); i ++) {
+            int h = height[i], h_idx = i;
+            while (!st.empty() && h > st.top().second) {
+                pair<int, int> p = st.top(); st.pop();
+                if (!st.empty()) {
+                    ans += (i - p.first) * (min(st.top().second, h) - p.second);
+                    h_idx = p.first;
+                }
+            }
+            if (st.empty() || h < st.top().second) {
+                st.push({h_idx, h});
+                continue;
+            }
+        }
+        return ans;
+    }
+};
